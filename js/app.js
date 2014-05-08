86edb3231694decaaee50fabea5d67f1abdb8158
t13689hn <t13689hn@zmac114.sfc.keio.ac.jp>
2014-05-08 12:52:07 +0900

    課題

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var inputoperandA = document.querySelector("#operand_a");
-var inputoperandB = document.querySelector("#operand_b");
+// var inputoperandB = document.querySelector("#operand_b");
 var inputOperator = document.querySelector("#operator");
 var output = document.querySelector("#output");
 var error = document.querySelector("#error");
@@ -16,29 +16,19 @@ var showResult = function(result){
 	output.value = result + "";
 };
 
-var add = function(a, b){
-	return a + b;
-};
-
-var subtract = function(a, b){
-	return a - b;
-};
 
 var multiply = function(a, b){
 	return a * b;
 };
 
-var divide = function(a, b){
-	return a / b;
-};
-
 var modulus = function(a, b){
 	return a % b;
-};
+}
+
+
 
 var isOperator = function(operator){
-	return operator == "+" || operator == "-" || operator == "*" ||
-	operator == "/" || operator == "%";
+	return operator == "*" || operator == "%";
 };
 
 var isNumber = function(a){
@@ -57,9 +47,7 @@ var isNaturalNumber = function(a){
 	return isInteger(a) && a > 0;
 };
 
-var isDivision = function(operator, a, b){
-	return operator == "/" && isNumber(a) && isNonZeroNumber(b);
-};
+
 
 var isModulus = function(operator, a, b){
 	return operator == "%" && isNaturalNumber(a) && isNaturalNumber(b);
@@ -69,22 +57,14 @@ var isMultiplication = function(operator, a, b){
 	return operator == "*" && isNumber(a) && isNumber(b);
 };
 
-var isSubtraction = function(operator, a, b){
-	return operator == "-" && isNumber(a) && isNumber(b);
-};
-
-var isAddition = function(operator, a, b){
-	return operator == "+" && isNumber(a) && isNumber(b);
-}
 
 var isReady = function(operator, a, b){
-	return isDivision(operator, a, b) || isModulus(operator, a, b) ||
-	isMultiplication(operator, a, b) || isSubtraction(operator, a, b) || isAddition(operator, a, b);
+	return  isModulus(operator, a, b) || isMultiplication(operator, a, b);
 };
 
 var startCalc = function(){
 	var operandA = inputoperandA.value;
-	var operandB = inputoperandB.value;
+	var operandB = 1.08;
 	var operator = inputOperator.value;
 
 	operandA = Number(operandA);
@@ -93,14 +73,8 @@ var startCalc = function(){
 	hideError();
 	if(isReady(operator, operandA, operandB)){
 		var result = 0;
-		if(operator == "+"){
-			result = add(operandA, operandB);
-		}else if(operator == "-"){
-			result = subtract(operandA, operandB)
-		}else if(operator == "*"){
+		if(operator == "*"){
 			result = multiply(operandA, operandB);
-		}else if(operator == "/"){
-			result = divide(operandA, operandB);
 		}else if(operator == "%"){
 			result = modulus(operandA, operandB);
 		}
